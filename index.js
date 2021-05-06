abddce225f322cdb0787af30ee4d4312d6e6fe72
Deshawn Monzon <inthemonzon@yahoo.com>
2021-05-06 18:19:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 function add() {
-  
+
 }
 function subtract() {
-  
+
 }
 function multiply() {
-  
+
 }
 function divide() {
-  
+
+}
+
+function add(3, 2) {
+
+}
+
+function subtract(10, 8) {
+
+}
+
+function multilpy(5, 5) {
+
+}
+
+function divide(12, 2) {
+
+}
+
+function increment(n) {
+
+}
+
+function decrement(n) {
+
 }

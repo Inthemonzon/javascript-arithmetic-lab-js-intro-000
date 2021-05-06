65426fde971914f9ec6fee75c45125d130812302
Deshawn Monzon <inthemonzon@yahoo.com>
2021-05-06 18:32:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-function add() {
-
-}
-function subtract() {
-
-}
-function multiply() {
-
-}
-function divide() {
-
+function add(a, b) {
+  return a + b
 }
 
-add(5, 5)
+function subtract(a, b) {
+  return a - b
+}
 
-subtract(a, b)
+function multiply(a, b) {
+  return a * b
+}
 
-multilpy(a, b)
-
-divide(a, b)
+function divide(a, b) {
+  return a / b
+}
 
 increment(n)
 
